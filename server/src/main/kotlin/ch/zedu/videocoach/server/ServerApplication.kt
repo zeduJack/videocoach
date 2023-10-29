9484e7e03788e29944d0f7545794ececce7fe9c8
Zeljko Dujmovic <zeljko.dujmovic83@gmail.com>
2023-10-29 18:55:40 +0100

    Init

---
+++ server/src/main/kotlin/ch/zedu/videocoach/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package ch.zedu.videocoach.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerApplication>(*args)
+}
