9fbaae2b36e43ad4ac9af65e574f03437d46a19e
Zeljko Dujmovic <zeljko.dujmovic83@gmail.com>
2024-01-08 16:15:43 +0100

    Reload

--- ui/src/app/hls/hls.component.ts
+++ ui/src/app/hls/hls.component.ts
@@ -1,45 +1,60 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import Hls from "hls.js";
 
 @Component({
-  selector: 'app-viewer',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './hls.component.html',
-  styleUrls: ['./hls.component.scss']
+    selector: 'app-viewer',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './hls.component.html',
+    styleUrls: ['./hls.component.scss']
 })
-export class HlsComponent  implements OnInit {
-  @ViewChild('video', { static: true }) videoElement!: ElementRef;
+export class HlsComponent implements OnInit {
+    @ViewChild('video', {static: true}) videoElement!: ElementRef;
 
-  private hls!: Hls;
-  private videoSrc = 'assets/hls/stream.m3u8'; // Replace with your HLS URL
+    private hls!: Hls;
+    private videoSrc = 'assets/hls/stream.m3u8'; // Replace with your HLS URL
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.setupVideoPlayer();
-  }
-
-  private setupVideoPlayer(): void {
-    const video: HTMLVideoElement = this.videoElement.nativeElement;
-
-    if (Hls.isSupported()) {
-      this.hls = new Hls();
-      this.hls.loadSource(this.videoSrc);
-      this.hls.attachMedia(video);
-      this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play();
-      });
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = this.videoSrc;
-      video.addEventListener('loadedmetadata', () => {
-        video.play();
-      });
+    constructor() {
     }
-  }
 
-  startHls() {
-    fetch("http://localhost:8080/starthls");
-  }
+    ngOnInit(): void {
+        this.setupVideoPlayer();
+    }
+
+    private setupVideoPlayer(): void {
+        const video: HTMLVideoElement = this.videoElement.nativeElement;
+
+        if (Hls.isSupported()) {
+            this.hls = new Hls();
+            this.hls.loadSource(this.videoSrc);
+            this.hls.attachMedia(video);
+            this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                video.play();
+            });
+
+            this.hls.on(Hls.Events.ERROR, this.handleError);
+        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+            video.src = this.videoSrc;
+            video.addEventListener('loadedmetadata', () => {
+                video.play();
+            });
+        }
+    }
+
+    handleError = (event: any, data: any): void => {
+        if (data.fatal) {
+            switch (data.type) {
+                default:
+                    setTimeout(() => {
+                        this.hls.destroy();
+                        this.setupVideoPlayer();
+                    }, 2000)
+            }
+        }
+    }
+
+    startHls() {
+        fetch("http://localhost:8080/starthls");
+    }
 }
