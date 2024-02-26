4ce74091b95b71b5933d254bd6392d06ecbbd95e
Zeljko Dujmovic <zeljko.dujmovic83@gmail.com>
2024-02-26 19:05:13 +0100

    Node with WebRTC

--- server/src/main/resources/static/server.js
+++ server/src/main/resources/static/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const socketIo = require('socket.io');
 
+console.log('key', __dirname + '/key.pem');
+
+const options = {
+  key: fs.readFileSync(__dirname + '/key.pem'),
+  cert: fs.readFileSync(__dirname + '/cert.pem')
+};
+
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 const io = socketIo(server);
 
 // Serve your static HTML files
 app.get('/sender', (req, res) => {
   res.sendFile(__dirname + '/sender.html');
 });
+
+app.get('/', (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+});
+
 
 app.get('/receiver', (req, res) => {
   res.sendFile(__dirname + '/receiver.html');
@@ -19,19 +33,28 @@ io.on('connection', socket => {
   console.log('A user connected');
 
   socket.on('create or join', room => {
+    room = '0';
     console.log('create or join to room ', room);
-    const myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
-    const numClients = myRoom.length;
+    //const myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
+    console.log('rooms', io.sockets.adapter.rooms);
+    const myRoom = io.sockets.adapter.rooms.get(room);
+    console.log('room', myRoom);
+
+    const numClients = myRoom ? myRoom.size : 0;
+    console.log(`The number of clients in the room ${room} is ${numClients}`);
 
     console.log(room, ' has ', numClients, ' clients');
 
     if (numClients === 0) {
+      console.log('creating room...');
       socket.join(room);
-      socket.emit('created', room);
+      socket.emit('join room', room);
     } else if (numClients === 1) {
+      console.log('joining room...');
       socket.join(room);
       socket.emit('joined', room);
     } else {
+      console.log('room is full...');
       socket.emit('full', room);
     }
   });
@@ -49,7 +72,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
